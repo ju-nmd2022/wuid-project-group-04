6747023b63d390ab894cc77304b37e7e472813e0
Liden119 <117270216+Liden119@users.noreply.github.com>
2022-12-06 10:28:30 +0100

    login update

--- js/main.js
+++ js/main.js
@@ -102,3 +102,34 @@ const myGames = document.getElementById("myGames-menu");
 menuIcon.addEventListener("click", function (event) {
   hamburgerMenu.style.display = "block";
 });
+
+//profile log in js
+
+let profile = document.getElementById("profile");
+let loginBox = document.getElementById("loginBox");
+let username = document.getElementById("username");
+let usernameHeader = document.getElementById("usernameHeader");
+let password = document.getElementById("password");
+let passwordHeader = document.getElementById("passwordHeader");
+let logInButton = document.getElementById("logInButton");
+let close = document.getElementById("close");
+
+profile.addEventListener("click", function (event) {
+  loginBox.style.display = "block";
+  username.style.display = "block";
+  usernameHeader.style.display = "block";
+  password.style.display = "block";
+  passwordHeader.style.display = "block";
+  logInButton.style.display = "block";
+  close.style.display = "block";
+});
+
+close.addEventListener("click", function (event) {
+  loginBox.style.display = "none";
+  username.style.display = "none";
+  usernameHeader.style.display = "none";
+  password.style.display = "none";
+  passwordHeader.style.display = "none";
+  logInButton.style.display = "none";
+  close.style.display = "none";
+});
--- js/myGames.js
+++ js/myGames.js
@@ -7,6 +7,35 @@ const community = document.getElementById("community-menu");
 const store = document.getElementById("store-menu");
 const myGames = document.getElementById("myGames-menu");
 
+let profile = document.getElementById("profile");
+let loginBox = document.getElementById("loginBox");
+let username = document.getElementById("username");
+let usernameHeader = document.getElementById("usernameHeader");
+let password = document.getElementById("password");
+let passwordHeader = document.getElementById("passwordHeader");
+let logInButton = document.getElementById("logInButton");
+let close = document.getElementById("close");
+
 menuIcon.addEventListener("click", function (event) {
   hamburgerMenu.style.display = "block";
+});
+
+profile.addEventListener("click", function (event) {
+  loginBox.style.display = "block";
+  username.style.display = "block";
+  usernameHeader.style.display = "block";
+  password.style.display = "block";
+  passwordHeader.style.display = "block";
+  logInButton.style.display = "block";
+  close.style.display = "block";
+});
+
+close.addEventListener("click", function (event) {
+  loginBox.style.display = "none";
+  username.style.display = "none";
+  usernameHeader.style.display = "none";
+  password.style.display = "none";
+  passwordHeader.style.display = "none";
+  logInButton.style.display = "none";
+  close.style.display = "none";
 });
